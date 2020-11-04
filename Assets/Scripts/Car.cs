cffb7415d8212e74b0ea9ea405bcffd599d40514
WolfBlackIvantsiv <54008883+WolfBlackIvantsiv@users.noreply.github.com>
2020-11-04 13:27:12 +0200

    Initial commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Car : MonoBehaviour
+{
+    public GameObject Target;
+    public float MaxSpeed;
+    private float Speed;
+    public float StopDistance;
+    public string CarTag;
+    public string LightControlTag;
+    public string LightControlRightTag;
+    public string LightControlLeftTag;
+    public string MarkerLeftTag;
+    public string MarkerRightTag;
+    public int Direction;
+    void Start()
+    {
+        Speed = MaxSpeed;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+
+        if (Target != null)
+        {
+            DirectionControl();
+        }
+        SpeedControl();
+        MoveToTarget();
+
+    }
+
+    void MoveToTarget()
+    {
+        this.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+    }
+
+    void DirectionControl()
+    {
+        this.transform.LookAt(Target.transform);
+    }
+
+    void SpeedControl()
+    {
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            if(hit.transform.tag == LightControlTag)
+            {
+                if(hit.transform.GetComponent<LightControler>().State == 0 && Vector3.Distance(this.transform.position,hit.transform.position) <= StopDistance)
+                {
+                    Speed = 0;
+                }
+                else if(hit.transform.GetComponent<LightControler>().State == 1)
+                {
+                    Speed = MaxSpeed;
+                }
+            }
+
+
+            if (hit.transform.tag == LightControlLeftTag)
+            {
+                if (hit.transform.GetComponent<LightControler>().State == 0 && Vector3.Distance(this.transform.position, hit.transform.position) <= StopDistance)
+                {
+                    Speed = 0;
+                }
+                else if (hit.transform.GetComponent<LightControler>().State == 1)
+                {
+                    Speed = MaxSpeed;
+                }
+            }
+
+
+            if (hit.transform.tag == LightControlRightTag)
+            {
+                if (hit.transform.GetComponent<LightControler>().State == 0 && Vector3.Distance(this.transform.position, hit.transform.position) <= StopDistance)
+                {
+                    Speed = 0;
+                }
+                else if (hit.transform.GetComponent<LightControler>().State == 1)
+                {
+                    Speed = MaxSpeed;
+                }
+            }
+
+
+            if (hit.transform.tag == CarTag)
+            {
+                if (Vector3.Distance(this.transform.position, hit.transform.position) <= StopDistance)
+                {
+                    Speed = 0;
+                }
+                else
+                {
+                    Speed = MaxSpeed;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == LightControlTag)
+        {
+            Direction = Random.Range(0, 2); // 0 - left, 1 - right
+            if (Direction == 0)
+            {
+                Target = other.gameObject.GetComponent<LightControler>().LeftMarker;
+            }
+
+            if (Direction == 1)
+            {
+                Target = other.gameObject.GetComponent<LightControler>().RightMarker;
+            }
+
+            if (Direction == 2)
+            {
+
+            }
+        }
+
+        if (other.tag == LightControlLeftTag)
+        {
+            Direction = 0;
+            if (Direction == 0)
+            {
+                Target = other.gameObject.GetComponent<LightControler>().LeftMarker;
+            }
+
+            if (Direction == 1)
+            {
+                Target = other.gameObject.GetComponent<LightControler>().RightMarker;
+            }
+        }
+
+        if (other.tag == LightControlRightTag)
+        {
+            Direction = 1;
+            if (Direction == 0)
+            {
+                Target = other.gameObject.GetComponent<LightControler>().LeftMarker;
+            }
+
+            if (Direction == 1)
+            {
+                Target = other.gameObject.GetComponent<LightControler>().RightMarker;
+            }
+        }
+
+        if (other.tag == MarkerLeftTag)
+        {
+            Target = null;
+            this.transform.rotation = other.transform.rotation;
+        }
+
+        if (other.tag == MarkerRightTag)
+        {
+            Target = null;
+            this.transform.rotation = other.transform.rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/LightControler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControler: MonoBehaviour
+{
+    public GameObject LeftMarker;
+    public GameObject RightMarker;
+    public GameObject Lamp;
+    public string LampTag;
+    public Material Material;
+    public float TimeChange = 10;
+    public float TimeRemaining;
+    public int State;
+    void Start()
+    {
+        Transform lamp = this.transform.Find(LampTag);
+        Lamp = lamp.gameObject;
+        TimeRemaining = TimeChange;
+        Material = Lamp.GetComponent<MeshRenderer>().material;
+        State = Random.Range(0, 2);
+        ChangeColor();
+    }
+
+    void Update()
+    {
+        Timer();
+    }
+
+    void ChangeColor()
+    { 
+        if(State == 0)
+        {
+            State = 1;
+            Material.color = Color.green;
+        }else if(State == 1)
+        {
+            State = 0;
+            Material.color = Color.red;
+        }
+
+    }
+
+
+    void Timer()
+    {
+        if (TimeRemaining > 0)
+        {
+            TimeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            ChangeColor();
+            TimeRemaining = TimeChange;
+        }
+    }
+
+}
